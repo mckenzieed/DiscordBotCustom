42675033b077501597d6fdfed6e0dc382123ea3a
Ethan McKenzie <edmckenzie@ua.edu>
2017-02-14 16:57:45 -0600

    basic functionality

---
+++ README.md
@@ -0,0 +1,28 @@
+# discord-guildrank-bot [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> 
+
+## Installation
+
+Clone this repository, and run:
+```sh
+$ npm install
+```
+
+## Usage
+
+```js
+$ npm run bot
+```
+## License
+
+Apache-2.0 © [Ethan McKenzie]()
+
+
+[npm-image]: https://badge.fury.io/js/discord-guildrank-bot.svg
+[npm-url]: https://npmjs.org/package/discord-guildrank-bot
+[travis-image]: https://travis-ci.org/mckenzieed/discord-guildrank-bot.svg?branch=master
+[travis-url]: https://travis-ci.org/mckenzieed/discord-guildrank-bot
+[daviddm-image]: https://david-dm.org/mckenzieed/discord-guildrank-bot.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/mckenzieed/discord-guildrank-bot
+[coveralls-image]: https://coveralls.io/repos/mckenzieed/discord-guildrank-bot/badge.svg
+[coveralls-url]: https://coveralls.io/r/mckenzieed/discord-guildrank-bot
---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+  // Your bot name. Typically, this is your bot's username without the discriminator.
+  // i.e: if your bot's username is MemeBot#0420, then this option would be MemeBot.
+  name: "guild-rank-bot",
+
+  // The bot's command prefix. The bot will recognize as command any message that begins with it.
+  // i.e: "-my discord bot foo" will trigger the command "foo",
+  //      whereas "My Discord Bot foo" will do nothing at all.
+  prefix:  "!",
+
+  // Your bot's user token. If you don't know what that is, go here:
+  // https://discordapp.com/developers/applications/me
+  // Then create a new application and grab your token.
+  token: "MjgwODMzOTg3NDU3NTgxMDU2.C4PKrg.tlIG1PyfyNmd4tIKB8grgrO5KK4",
+
+  // If this option is enabled, the bot will delete the message that triggered it, and its own
+  // response, after the specified amount of time has passed.
+  // Enable this if you don't want your channel to be flooded with bot messages.
+  // ATTENTION! In order for this to work, you need to give your bot the following permission:
+  // MANAGE_MESSAGES - 	0x00002000
+  // More info: https://discordapp.com/developers/docs/topics/permissions
+  deleteAfterReply: {
+    enabled: false,
+    time: 10000, // In milliseconds
+  }
+};
